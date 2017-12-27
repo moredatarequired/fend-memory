09dd2f6f0d7b31fdf9526094c8876ff08e739ebb
Hugh <hugh.wimberly@gmail.com>
2017-12-27 11:24:57 -0800

    Shuffle the cards.

--- js/app.js
+++ js/app.js
@@ -1,30 +1,18 @@
-/*
- * Create a list that holds all of your cards
- */
+// The cards that the player tries to match.
+const cards = document.querySelectorAll('.card');
 
+// Use the Flexbox 'order' property to shuffle display order.
+let flexOrder = shuffle([...Array(16).keys()]);
+cards.forEach((card, index) => card.style.order = flexOrder[index]);
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
+// shuffle function from https://stackoverflow.com/questions/6274339
+function shuffle(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
     }
-
-    return array;
+    return a;
 }
-
 
 /*
  * set up the event listener for a card. If a card is clicked:
@@ -36,7 +24,6 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
-let cards = document.querySelectorAll('.card');
 let openCard = null;
 function cardClick(e) {
     let classes = e.target.classList;
